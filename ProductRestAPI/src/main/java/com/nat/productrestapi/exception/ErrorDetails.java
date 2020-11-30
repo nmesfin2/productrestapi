78ac19337aef1ec1b05170cf35326bdd51f259f3
nat <nmexodus870@gmail.com>
2020-11-30 16:10:52 +0000

    global exception handling implemented

---
+++ ProductRestAPI/src/main/java/com/nat/productrestapi/exception/ErrorDetails.java
@@ -0,0 +1,16 @@
+package com.nat.productrestapi.exception;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+	private Date date;
+	private String message;
+	private String details;
+}
